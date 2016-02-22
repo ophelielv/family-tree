d127b535a37a1e8049c65ab9c44b8bc3a1c8124d
ophelielv <levigouroux.oph@gmail.com>
2016-02-22 00:26:14 +0000

    authentification en cours, version 2.5 de symfony, il faut upgrader

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Oph\FamilytreeBundle\OphFamilytreeBundle(),
+            new Oph\UserBundle\OphUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Oph/UserBundle/OphUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oph\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OphUserBundle extends Bundle
+{
+}
